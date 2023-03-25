52b32cc308634bad63197949f85d849cca56445d
lalitmore1 <morelalit835@gmail.com>
2023-03-25 15:32:00 +0530

    Added Demo

---
+++ Demo/src/com/wipro/Demo.java
@@ -0,0 +1,16 @@
+package com.wipro;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList <String> list = new ArrayList <String>();
+        list.add("Lalit");
+        list.add("Amol");
+        list.add("Sai");
+        list.add("Dhanraj");
+        System.out.println(list);
+	}
+
+}
